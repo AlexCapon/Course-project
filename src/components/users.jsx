9584cc01ba7c80a811750c2d8ab539e91222467f
AlexCapon <125947007+AlexCapon@users.noreply.github.com>
2023-09-24 23:41:41 +0700

    1.User list render from fetch is completed 2.Handle click on delete-button is completed 3.Change of status badge is completed

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,16 +1,85 @@
 import React from "react";
 import api from "../api";
 import { useState } from "react";
-// import { fetchAll } from "../api/fake.api/user.api";
+
+// Инициализируем статусный текст
+const fetchedUsers = api.users.fetchAll();
+let statusText = <span className="badge bg-primary m-3">{fetchedUsers.length} people will party with you</span>
 
 function Users() {
-  console.log(api.users.fetchAll());
   const [users, setUser] = useState(api.users.fetchAll());
-  function handleDelete(userId) {}
-  function handlePhrase(number) {}
+  // Обрабатываем нажатие на кнопку
+  function handleDelete(userId) {
+    setUser((prevState) => {
+      handlePhrase(users.length - 1);
+      return prevState.filter((user) => user._id !== userId)
+    })
+  }
+  // Отрабатываем изменение статусного текста
+  function handlePhrase(number) {
+  let usersLeft = number;
+  statusText =
+  usersLeft === 0 ? (
+    <span className="badge bg-danger m-3">Nobody will party with you</span>
+  ) : (
+    <span className="badge bg-primary m-3">
+      {usersLeft} people will party with you
+    </span>
+  );
+  }
+  // Строим голову таблицы
+function buildTableHead() {
+  return (
+    <thead>
+      <tr>
+        <th scope="col">Имя</th>
+        <th scope="col">Качества</th>
+        <th scope="col">Профессия</th>
+        <th scope="col">Встретился, раз</th>
+        <th scope="col">Оценка</th>
+        <th scope="col"></th>
+      </tr>
+    </thead>
+  );
+}
+// Строим строку в таблице
+function buildTableRow(user) {
+  // Расставляем качества
+  function buildQualities() {
+    return user.qualities.map((quality) => (
+      <span key={quality.name} className={"badge  m-1 bg-" + quality.color}>
+        {quality.name}
+      </span>
+    ));
+  }
+
+  return (
+    <tr id={user._id} key={user._id + "2"}>
+      <td scope="col">{user.name}</td>
+      <td scope="col">{buildQualities()}</td>
+      <td scope="col">{user.profession.name}</td>
+      <td scope="col">{user.completedMeetings}</td>
+      <td scope="col">{user.rate}</td>
+      <td scope="col">
+        <button id={user._id + "del_btn"} className="btn btn-danger" onClick={() => handleDelete(user._id)}>
+          delete
+        </button>
+      </td>
+    </tr>
+  );
+}
+// Строим тело таблицы
+function buildTableBody(users) {
+  return <tbody>{users.map((user) => buildTableRow(user))}</tbody>;
+}
+// Рендерим список
   return (
     <>
-      <h1>Users</h1>
+      <h1>{statusText}</h1>
+      <table className="table">
+        {buildTableHead()}
+        {buildTableBody(users)}
+      </table>
     </>
   );
 }
