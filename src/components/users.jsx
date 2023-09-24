5dd642de1d164fe1fbd366eaf2ae2716acbc6402
AlexCapon <125947007+AlexCapon@users.noreply.github.com>
2023-09-24 23:57:01 +0700

    1. Optimized changing of status text. 2. Removing table if no one left to party.

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -2,77 +2,86 @@ import React from "react";
 import api from "../api";
 import { useState } from "react";
 
-// Инициализируем статусный текст
-const fetchedUsers = api.users.fetchAll();
-let statusText = <span className="badge bg-primary m-3">{fetchedUsers.length} people will party with you</span>
-
 function Users() {
   const [users, setUser] = useState(api.users.fetchAll());
-  // Обрабатываем нажатие на кнопку
-  function handleDelete(userId) {
-    setUser((prevState) => {
-      handlePhrase(users.length - 1);
-      return prevState.filter((user) => user._id !== userId)
-    })
-  }
-  // Отрабатываем изменение статусного текста
-  function handlePhrase(number) {
-  let usersLeft = number;
-  statusText =
-  usersLeft === 0 ? (
+  // Инициализируем статусный текст
+  let statusText =
+  users.length === 0 ? (
     <span className="badge bg-danger m-3">Nobody will party with you</span>
   ) : (
     <span className="badge bg-primary m-3">
-      {usersLeft} people will party with you
+      {users.length} people will party with you
     </span>
   );
+  // Обрабатываем нажатие на кнопку
+  function handleDelete(userId) {
+    setUser((prevState) => {
+      handlePhrase();
+      return prevState.filter((user) => user._id !== userId);
+    });
+  }
+  // Отрабатываем изменение статусного текста
+  function handlePhrase() {
+    statusText =
+    users.length === 0 ? (
+        <span className="badge bg-danger m-3">Nobody will party with you</span>
+      ) : (
+        <span className="badge bg-primary m-3">
+          {users.length} people will party with you
+        </span>
+      );
   }
   // Строим голову таблицы
-function buildTableHead() {
-  return (
-    <thead>
-      <tr>
-        <th scope="col">Имя</th>
-        <th scope="col">Качества</th>
-        <th scope="col">Профессия</th>
-        <th scope="col">Встретился, раз</th>
-        <th scope="col">Оценка</th>
-        <th scope="col"></th>
-      </tr>
-    </thead>
-  );
-}
-// Строим строку в таблице
-function buildTableRow(user) {
-  // Расставляем качества
-  function buildQualities() {
-    return user.qualities.map((quality) => (
-      <span key={quality.name} className={"badge  m-1 bg-" + quality.color}>
-        {quality.name}
-      </span>
-    ));
+  function buildTableHead() {
+    return (
+      <thead>
+        <tr>
+          <th scope="col">Имя</th>
+          <th scope="col">Качества</th>
+          <th scope="col">Профессия</th>
+          <th scope="col">Встретился, раз</th>
+          <th scope="col">Оценка</th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+    );
   }
+  // Строим строку в таблице
+  function buildTableRow(user) {
+    // Расставляем качества
+    function buildQualities() {
+      return user.qualities.map((quality) => (
+        <span key={quality.name} className={"badge  m-1 bg-" + quality.color}>
+          {quality.name}
+        </span>
+      ));
+    }
 
-  return (
-    <tr id={user._id} key={user._id + "2"}>
-      <td scope="col">{user.name}</td>
-      <td scope="col">{buildQualities()}</td>
-      <td scope="col">{user.profession.name}</td>
-      <td scope="col">{user.completedMeetings}</td>
-      <td scope="col">{user.rate}</td>
-      <td scope="col">
-        <button id={user._id + "del_btn"} className="btn btn-danger" onClick={() => handleDelete(user._id)}>
-          delete
-        </button>
-      </td>
-    </tr>
-  );
-}
-// Строим тело таблицы
-function buildTableBody(users) {
-  return <tbody>{users.map((user) => buildTableRow(user))}</tbody>;
-}
-// Рендерим список
+    return (
+      <tr id={user._id} key={user._id + "2"}>
+        <td scope="col">{user.name}</td>
+        <td scope="col">{buildQualities()}</td>
+        <td scope="col">{user.profession.name}</td>
+        <td scope="col">{user.completedMeetings}</td>
+        <td scope="col">{user.rate}</td>
+        <td scope="col">
+          <button
+            id={user._id + "del_btn"}
+            className="btn btn-danger"
+            onClick={() => handleDelete(user._id)}
+          >
+            delete
+          </button>
+        </td>
+      </tr>
+    );
+  }
+  // Строим тело таблицы
+  function buildTableBody(users) {
+    return <tbody>{users.map((user) => buildTableRow(user))}</tbody>;
+  }
+  // Рендерим список
+  if (users.length === 0) return <h1>{statusText}</h1>;
   return (
     <>
       <h1>{statusText}</h1>
